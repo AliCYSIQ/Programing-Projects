70c60c15c7fb1a6fa36ea9fe3a6e81b9eb23d75e
ilent0 <laa22132@gmail.com>
2026-03-18 16:33:58 +0300

    make player baddel move

--- Python/Pong/Pong.py
+++ Python/Pong/Pong.py
@@ -9,6 +9,25 @@ clock = pg.time.Clock()
 oppentBaddel = pg.Rect(350, 10, 100, 10)
 playerBaddel = pg.Rect(350, 630, 100, 10)
 
+
+def PlayerBaddelMovemnt():
+    keys = pg.key.get_pressed()
+    speed = 5
+    if keys[pg.K_LSHIFT]:
+        speed = 10
+    else:
+        speed = 5
+    if keys[pg.K_a]:
+        playerBaddel.x -= speed
+    elif keys[pg.K_d]:
+        playerBaddel.x += speed
+
+    if playerBaddel.left < 0:
+        playerBaddel.left = 0
+    elif playerBaddel.right > 540:
+        playerBaddel.right = 540
+
+
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:  # If they click the 'X'
@@ -16,6 +35,8 @@ while True:
             sys.exit()
 
     screen.fill((30, 30, 46))  # Fill the screen with a dark color (RGB)
+
+    PlayerBaddelMovemnt()
 
     pg.draw.rect(surface=screen, color=(255, 255, 255), rect=playerBaddel)
     pg.draw.rect(surface=screen, color=(255, 255, 255), rect=oppentBaddel)

17bd1d77add6cea763570f5671648869b476cdca
ilent0 <laa22132@gmail.com>
2026-03-20 21:43:50 +0300

    ai for Opponent

--- Python/Pong/Pong.py
+++ Python/Pong/Pong.py
@@ -13,11 +13,15 @@ clock = pg.time.Clock()
 oppentBaddel = pg.Rect(230, 10, 100, 10)
 playerBaddel = pg.Rect(230, 630, 100, 10)
 ball = pg.Rect(Xscreen / 2, Yscreen / 2, 10, 10)
-ball_speed_x = 5 + random.randint(-1, 1)
-ball_speed_y = 5 + random.randint(-1, 1)
+ball_speed_x = 0
+ball_speed_y = 0
 playerPoint = 0
 oppentPoint = 0
-
+scoreTime = 0
+isBallActive = False
+ballStopTime = 500
+ball_speed_x = 0
+ball_speed_y = 0
 
 def PlayerBaddelMovemnt():
     keys = pg.key.get_pressed()
@@ -38,19 +42,34 @@ def PlayerBaddelMovemnt():
 
 
 def MoveBall():
-    global ball_speed_x, ball_speed_y, oppentPoint, playerPoint
+    global ball_speed_x, ball_speed_y, oppentPoint, playerPoint,scoreTime,isBallActive
+    
+    if not isBallActive:
+        currentTime = pg.time.get_ticks()
+       
+        if currentTime - scoreTime >= ballStopTime:
+            isBallActive = True
+            ball_speed_x = 5 + random.randint(-2, 2)
+            ball_speed_y = 5 + random.randint(-1, 1)
+        return
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top < 0:
         ball.x = Xscreen / 2
         ball.y = Yscreen / 2
-
         playerPoint += 1
+        isBallActive = False
+        scoreTime = pg.time.get_ticks()
+        ball_speed_y *= -1
+        
     elif ball.bottom > Yscreen:
         ball.x = Xscreen / 2
         ball.y = Yscreen / 2
         oppentPoint += 1
+        ball_speed_y *= -1
+        isBallActive = False
+        scoreTime = pg.time.get_ticks()
 
     if ball.left <= 0 or ball.right > Xscreen:
         ball_speed_x *= -1
@@ -64,34 +83,48 @@ def MoveBall():
 
     screen.blit(text_surface, (ball.x + 3, ball.y + 3))
 
-
+def OpponentMovement():
+    freeOffsit =5
+    aiSpeed = 4
+    if(ball.y > (Yscreen/2)+freeOffsit):
+        aiSpeed = 2 # to make it easier and more fair to the player 
+    else:
+        aiSpeed = 4
+    if ball.centerx > oppentBaddel.centerx+freeOffsit:
+        oppentBaddel.x +=aiSpeed
+    elif ball.centerx < oppentBaddel.centerx+freeOffsit:
+        oppentBaddel.x -=aiSpeed
 # Pass None for the font file argument
 SPEED_FONT = pg.font.Font(None, 25)
 POINT_FONT = pg.font.Font(None, 30)
 
 
 while True:
+    # 1. INPUT
     for event in pg.event.get():
-        if event.type == pg.QUIT:  # If they click the 'X'
+        if event.type == pg.QUIT:
             pg.quit()
             sys.exit()
 
-    screen.fill((30, 30, 46))  # Fill the screen with a dark color (RGB)
+    # 2. Movement
+    PlayerBaddelMovemnt()
+    OpponentMovement() 
+    MoveBall()
 
-    ## draw
+    # 3. DRAW
+    screen.fill((30, 30, 46))
     pg.draw.ellipse(surface=screen, color=(255, 0, 0), rect=ball)
     pg.draw.rect(surface=screen, color=(255, 255, 255), rect=playerBaddel)
     pg.draw.rect(surface=screen, color=(255, 255, 255), rect=oppentBaddel)
-
-    ## move
-
-    MoveBall()
-
+    
     point_text = POINT_FONT.render(f"{playerPoint}:{oppentPoint}", True, (255, 255, 0))
     screen.blit(point_text, (Xscreen - 50, Yscreen / 2))
-    PlayerBaddelMovemnt()
 
+    speed_text = SPEED_FONT.render(
+        f"{math.hypot(ball_speed_x, ball_speed_y):.1f} cm/s", True, (255, 255, 0)
+    )  # White text
+    screen.blit(speed_text, (ball.x + 3, ball.y + 3))
+
+    # 4. FLIP & CLOCK
     pg.display.flip()
-
-    # Force the loop to run at exactly 60 Frames Per Second
     clock.tick(60)

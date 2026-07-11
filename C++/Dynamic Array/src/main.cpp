32c6be1ae47d6c630b832caf8594873d0e37dd9e
ilent0 <alicys0cyberbox@gmail.com>
2026-07-12 00:19:34 +0300

    Dynamic Array: done (have Issues)

--- C++/Dynamic Array/src/main.cpp
+++ C++/Dynamic Array/src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string>
 
-// TODO: add struct that take any number of vars and push back to the DynArray in one line
 template <typename T>
 class DynArray {
    private:
@@ -10,6 +9,17 @@ class DynArray {
     size_t capacity = 0;
 
    public:
+    void ReAlocate() {
+        T* oldData = data;
+        data = nullptr;
+        data = new T[capacity];
+
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = oldData[i];
+        }
+        delete[] oldData;
+        oldData = nullptr;
+    }
     DynArray() {
         data = nullptr;
         size = 0;
@@ -35,17 +45,8 @@ class DynArray {
                 return;
             }
             capacity = (capacity * 2) - 1;
-            T* oldData = data;
-            data = nullptr;
-            data = new T[capacity];
-
-            for (size_t i = 0; i < size; ++i) {
-                data[i] = oldData[i];
-            }
-            delete[] oldData;
-            oldData = nullptr;
+            ReAlocate();
         }
-
         data[size] = value;
         size++;
     }
@@ -59,21 +60,87 @@ class DynArray {
                         // that's already exists
         return data[i];
     }
+    DynArray& operator=(std::initializer_list<T> list) {
+        delete[] data;
+        size = capacity = list.size();
+        data = new T[capacity];
+        for (size_t i = 0; i < size; i++) {
+            data[i] = *(list.begin() + i);
+        }
+        return *this;
+    }
+    bool IsEmpty() { return size == 0; }
+    void Reserve(size_t cap) {
+        if (cap <= capacity)
+            return;
+        capacity = cap;
+        ReAlocate();
+    }
+    T Front() { return data[0]; }
+    T Back() { return data[size - 1]; }
+    T PopBack() {
+        if (size > 0)
+            size--;
+        return data[size];
+    }
+    void Print() {
+        std::cout << std::endl;
+        for (size_t i = 0; i < size; i++) {
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    void Print(size_t index) {
+        if (index >= size) {
+            index = size;
+        }
+        std::cout << std::endl;
+        for (size_t i = 0; i < index; i++) {
+            std::cout << data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    void Insert(size_t index, const T& value) {
+        if (index > size || index < 0)
+            return;
+        size++;
+        if (size >= capacity) {
+            capacity = capacity * 2 - 1;
+            ReAlocate();
+        }
+
+        for (size_t i = size - 1; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = value;
+    }
+    void Erase(size_t index) {
+        if (index > size || index < 0)
+            return;
+
+        size--;
+        for (size_t i = index; i < size; i++) {
+            data[i] = data[i + 1];
+        }
+    }
+    void Clear() { size = 0; }
+    T* Begin() { return data; }
+    T* End() { return data + size; }
 };
 
 int main() {
     DynArray<int> a;
     a.PushBack(5);
-    a.PushBack(3);
-    a.PushBack(10);
-    a.PushBack(8);
-    a.PushBack(9);
-    a.PushBack(7);
-    a.PushBack(6);
-    std::cout << a.GetLast() << std::endl;
+    a = {5, 10, 20, 32, 453, 543};
+    a.Print();
     std::cout << a.Size() << std::endl;
-    std::cout << a[-5] << std::endl;
-    std::cout << a.Capacity();
+    a.Erase(1);
+    a.Print();
+    std::cout << a.Size() << std::endl;
+    std::cout << a.Capacity() << std::endl;
+    a.Clear();
+    std::cout << a.Size() << std::endl;
+    std::cout << a.Capacity() << std::endl;
 
     return 0;
 }
